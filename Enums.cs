6da4e3c90e3e72eadcea02b0f51ee97e270935f1
Арсений <solomonlol95@gmail.com>
2026-07-11 14:33:39 +0300

    Релиз игры

---
+++ Enums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STUDY.MathGame
+{
+    internal class Enums
+    {
+        internal enum Menu
+        {
+            StartGame,
+            ViewHistory,
+            CleanHistory
+        }
+        internal enum Operation
+        {
+            Addition ='+',
+            Substraction='-',
+            Multiplication='*',
+            Division='/'
+        }
+        internal enum Difficulty
+        {
+            Easy=10,
+            Medium=100,
+            Hard=1000
+        }
+    }
+}
